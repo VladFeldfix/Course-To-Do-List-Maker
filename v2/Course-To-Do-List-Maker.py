19115220d82a398e38a84e2ef707959d9438ba89
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-06-28 19:50:48 +0300

    general sync

---
+++ v2/Course-To-Do-List-Maker.py
@@ -0,0 +1,37 @@
+from SmartConsole import *
+
+class main:
+    # constructor
+    def __init__(self):
+        # load smart console
+        self.sc = SmartConsole("APA Maker", "2.0")
+
+        # set-up main memu
+        self.sc.main_menu["RUN"] = self.run
+
+        # start
+        self.sc.start()
+    
+    def run(self):
+        course_name = self.sc.input("What is the course code?").upper()
+        for i in range(1,9):
+            print("%s UN0%s - check grades for previous week" % (course_name,i))
+            print("%s UN0%s - reading assignment" % (course_name,i))
+            print("%s UN0%s - written / programming assignment" % (course_name,i))
+            print("%s UN0%s - discussion assignment" % (course_name,i))
+            print("%s UN0%s - self quiz" % (course_name,i))
+            print("%s UN0%s - graded quiz" % (course_name,i))
+            print("%s UN0%s - peer assessment 1" % (course_name,i))
+            print("%s UN0%s - peer assessment 2" % (course_name,i))
+            print("%s UN0%s - peer assessment 3" % (course_name,i))
+            print("%s UN0%s - rate and comment 1" % (course_name,i))
+            print("%s UN0%s - rate and comment 2" % (course_name,i))
+            print("%s UN0%s - rate and comment 3" % (course_name,i))
+            print("%s UN0%s - learning journal" % (course_name,i))
+            print("--------------------------------------------------------------------------------------")
+        print("%s UN09 - final exam" % (course_name))
+
+        # restart
+        self.sc.restart()
+
+main()
