5cfce872485b34d14631ad8a2480f3b9c24e355b
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-01-18 21:37:01 +0200

    general sync

--- Course-To-Do-List-Maker.py
+++ Course-To-Do-List-Maker.py
@@ -4,7 +4,7 @@ class main:
     # constructor
     def __init__(self):
         # load smart console
-        self.sc = SmartConsole("Course To Do List Maker", "2.0")
+        self.sc = SmartConsole("Course To Do List Maker", "1.0")
 
         # set-up main memu
         self.sc.add_main_menu_item("RUN", self.run)
